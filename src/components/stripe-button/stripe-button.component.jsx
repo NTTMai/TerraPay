5aa0d7b6f4ab43921eee0a1271f997b7db466115
NTPhuong <thaiphuongn1@gmail.com>
2020-06-19 02:21:20 +0700

    logo

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,3 +2,31 @@ import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 
 import "./stripe-button.styles.scss";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51GvHwRJ3w2P8I8zSl6GbRZVT3mx8RcSQPJRx27NfrpZqwqsWx4UgIwWFwx8Qn6M40iUG9efRBtluCJliK491ROU300CagXTLRl";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
