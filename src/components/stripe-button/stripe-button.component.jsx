34a3110f98ddc7fde5f289895dde03b89193a5a0
NTPhuong <thaiphuongn1@gmail.com>
2020-06-19 01:12:19 +0700

    first commit

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+import "./stripe-button.styles.scss";
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBsu96uw4NzWcOxf9kJhoimpPaMTmzyDrI",
+  authDomain: "crwn-db-9b31f.firebaseapp.com",
+  databaseURL: "https://crwn-db-9b31f.firebaseio.com",
+  projectId: "crwn-db-9b31f",
+  storageBucket: "crwn-db-9b31f.appspot.com",
+  messagingSenderId: "1052458345029",
+  appId: "1:1052458345029:web:44f12266deceabc4ff392c",
+  measurementId: "G-SDMM7XGV8L",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const terraWallet = "0x123lkjasdf";
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        terraWallet,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
